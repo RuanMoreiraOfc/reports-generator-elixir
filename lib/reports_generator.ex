c4ff741dcd8a1c948d14f5e1639e0ff39e8cb310
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-10 20:11:51 -0300

    refactor: rename `sum_prices` to a generic name

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -4,12 +4,12 @@ defmodule ReportsGenerator do
   def build(file) do
     file
     |> Parser.parse_file()
-    |> Enum.reduce(init_report_acc(30), &sum_prices/2)
+    |> Enum.reduce(init_report_acc(30), &sum_values/2)
   end
 
   def retrieve_higher_cost(report), do: Enum.max_by(report, fn {_key, value} -> value end)
 
-  defp sum_prices([id, _name, price], acc), do: Map.put(acc, id, acc[id] + price)
+  defp sum_values([id, _food_name, price], acc), do: Map.put(acc, id, acc[id] + price)
 
   defp init_report_acc(maxId), do: Enum.into(1..maxId, %{}, &{Integer.to_string(&1), 0})
 end
