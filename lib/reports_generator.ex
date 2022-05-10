e317ecc7c162985e1fb882a8dbba0c1d1362bd31
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-10 19:23:39 -0300

    feat: return total price grouped by id

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,10 +1,21 @@
 defmodule ReportsGenerator do
   def build(filename) do
     "reports/#{filename}"
-    |> File.read()
-    |> handle_file
+    |> File.stream!()
+    |> Enum.reduce(%{}, fn line, acc ->
+      [id, _name, price] = parse_line(line)
+
+      oldPrice = acc[id] || 0
+      newPrice = oldPrice + price
+
+      Map.put(acc, id, newPrice)
+    end)
   end
 
-  defp handle_file({:ok, content}), do: content
-  defp handle_file({:error, _reason}), do: "Error while processing file!"
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2, &String.to_integer/1)
+  end
 end
