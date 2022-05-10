85913ddb97f7ddce0f755f17769281bb34bab53e
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-05-10 20:04:59 -0300

    feat: add `retrieve_higher_cost` function

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -7,6 +7,8 @@ defmodule ReportsGenerator do
     |> Enum.reduce(init_report_acc(30), &sum_prices/2)
   end
 
+  def retrieve_higher_cost(report), do: Enum.max_by(report, fn {_key, value} -> value end)
+
   defp sum_prices([id, _name, price], acc), do: Map.put(acc, id, acc[id] + price)
 
   defp init_report_acc(maxId), do: Enum.into(1..maxId, %{}, &{Integer.to_string(&1), 0})
